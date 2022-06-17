34de967c320fc84fd04cc711114051e7a38d3c63
gabe <gconcepci@gmail.com>
2022-06-16 23:46:28 -0400

    Complete project

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const choices = ["rock", "paper", "scissors"];
-
 function computerSelect() {
     let randInt = Math.floor(Math.random() * choices.length);
     return choices[randInt];
@@ -18,34 +16,69 @@ function playRound(playerChoice, computerChoice) {
         return "That is a DRAW!"
     } else if (playerChoice === "rock") {
         if (computerChoice === "paper") {
+            computerScore++
             return computerWin;
         }
         else {
+            playerScore++
             return playerWin;
         }
     } else if (playerChoice === "paper") {
         if (computerChoice === "scissors") {
+            computerScore++
             return computerWin; 
         }
         else {
+            playerScore++
             return playerWin;
         }
     } else if (playerChoice === "scissors") {
         if (computerChoice === "rock") {
+            computerScore++
             return computerWin;
         }
         else {
+            playerScore++
             return playerWin;
         } 
     }
 }
 
 function testRound() {
-    let cpu = computerSelect();
     let player = computerSelect();
+    let cpu = computerSelect();
     return playRound(player, cpu);
 }
 
-for (let i = 0; i < 20; i++) {
-    console.log(testRound())
+function game() {
+    let cpu = computerSelect();
+    let player = playerSelect();
+    let result = playRound(player,cpu);
+    console.log(result);
+    displayScores();
 }
+
+function displayScores() {
+    console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`);
+}
+
+function displayWinner() {
+    if (playerScore === 3) {
+        alert("Player has won!");
+    } else if (computerScore === 3) 
+    {
+        alert("Computer has won!");
+    }
+}
+
+
+const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+// GAME LOGIC
+
+while (playerScore < 3 && computerScore < 3) {
+    game();
+    displayWinner();
+} 
