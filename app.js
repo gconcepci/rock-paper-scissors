337eefe60beed6b81e4bb1e1daad0ebb2a36184a
gabe <gconcepci@gmail.com>
2022-06-19 21:25:23 -0400

    Refactored playerSelect and computerSelect functions

--- app.js
+++ app.js
@@ -1,84 +1,92 @@
-// function computerSelect() {
-//     let randInt = Math.floor(Math.random() * choices.length);
-//     return choices[randInt];
-// }
+const cards = document.querySelectorAll(".card");
+const choices = ["rock", "paper", "scissors"];
 
-// function playerSelect() {
-//     let choice = prompt("Choose 'rock', 'paper', or 'scissors': ").toLowerCase();
-//     return choice
-// }
-
-// function playRound(playerChoice, computerChoice) {
-//     let playerWin = `Player wins! ${playerChoice} beats ${computerChoice}!`;
-//     let computerWin = `Computer wins! ${computerChoice} beats ${playerChoice}!`;
-
-//     if (playerChoice === computerChoice) {
-//         return "That is a DRAW!"
-//     } else if (playerChoice === "rock") {
-//         if (computerChoice === "paper") {
-//             computerScore++
-//             return computerWin;
-//         }
-//         else {
-//             playerScore++
-//             return playerWin;
-//         }
-//     } else if (playerChoice === "paper") {
-//         if (computerChoice === "scissors") {
-//             computerScore++
-//             return computerWin; 
-//         }
-//         else {
-//             playerScore++
-//             return playerWin;
-//         }
-//     } else if (playerChoice === "scissors") {
-//         if (computerChoice === "rock") {
-//             computerScore++
-//             return computerWin;
-//         }
-//         else {
-//             playerScore++
-//             return playerWin;
-//         } 
-//     }
-// }
-
-// function testRound() {
-//     let player = computerSelect();
-//     let cpu = computerSelect();
-//     return playRound(player, cpu);
-// }
-
-// function game() {
-//     let cpu = computerSelect();
-//     let player = playerSelect();
-//     let result = playRound(player,cpu);
-//     console.log(result);
-//     displayScores();
-// }
-
-// function displayScores() {
-//     console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`);
-// }
-
-// function displayWinner() {
-//     if (playerScore === 3) {
-//         alert("Player has won!");
-//     } else if (computerScore === 3) 
-//     {
-//         alert("Computer has won!");
-//     }
-// }
+cards.forEach(card =>{
+    card.addEventListener("click", computerSelect);
+    card.addEventListener("click", playerSelect);
+    card.addEventListener("click", playRound);
+});
 
 
-// const choices = ["rock", "paper", "scissors"];
-// let playerScore = 0;
-// let computerScore = 0;
+function computerSelect() {
+    let randInt = Math.floor(Math.random() * choices.length);
+    let choice = choices[randInt];
+    console.log(choice)
+    return choice;
+}
 
-// // GAME LOGIC
+function playerSelect() {
+    let choice = this.dataset.choice;
+    return choice
+}
 
-// while (playerScore < 3 && computerScore < 3) {
-//     game();
-//     displayWinner();
-// } 
+function playRound(playerChoice, computerChoice) {
+    let playerWin = `Player wins! ${playerChoice} beats ${computerChoice}!`;
+    let computerWin = `Computer wins! ${computerChoice} beats ${playerChoice}!`;
+    
+
+    if (playerChoice === computerChoice) {
+        return "That is a DRAW!"
+    } else if (playerChoice === "rock") {
+        if (computerChoice === "paper") {
+            computerScore++
+            return computerWin;
+        }
+        else {
+            playerScore++
+            return playerWin;
+        }
+    } else if (playerChoice === "paper") {
+        if (computerChoice === "scissors") {
+            computerScore++
+            return computerWin; 
+        }
+        else {
+            playerScore++
+            return playerWin;
+        }
+    } else if (playerChoice === "scissors") {
+        if (computerChoice === "rock") {
+            computerScore++
+            return computerWin;
+        }
+        else {
+            playerScore++
+            return playerWin;
+        } 
+    }
+}
+
+function testRound() {
+    let player = computerSelect();
+    let cpu = computerSelect();
+    return playRound(player, cpu);
+}
+
+function game() {
+    let cpu = computerSelect();
+    let player = playerSelect();
+    let result = playRound(player,cpu);
+    console.log(result);
+    displayScores();
+}
+
+function displayScores() {
+    console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`);
+}
+
+function displayWinner() {
+    if (playerScore === 3) {
+        alert("Player has won!");
+    } else if (computerScore === 3) 
+    {
+        alert("Computer has won!");
+    }
+}
+
+
+
+let playerScore = 0;
+let computerScore = 0;
+
+// GAME LOGIC
