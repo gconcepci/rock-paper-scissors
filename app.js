4044680ff78a9afc6c62b0bacc3c24a8f4a1b547
gabe <gconcepci@gmail.com>
2022-06-20 22:43:41 -0400

    Remake UI

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const cards = document.querySelectorAll(".card");
 const choices = ["rock", "paper", "scissors"];
+const results = document.querySelector(".results");
 
-cards.forEach(card =>{
-    card.addEventListener("click", computerSelect);
-    card.addEventListener("click", playerSelect);
-    card.addEventListener("click", playRound);
-});
-
+function displayResult(text) {
+    let p = document.createElement("p");
+    p.textContent = text;
+    results.appendChild(p)
+}
 
 function computerSelect() {
     let randInt = Math.floor(Math.random() * choices.length);
     let choice = choices[randInt];
-    console.log(choice)
+    displayResult(`Computer played ${choice}!`);
     return choice;
 }
 
-function playerSelect() {
+let playerChoice = function playerSelect() {
     let choice = this.dataset.choice;
+    displayResult(`Player played ${choice}!`);
     return choice
 }
 
-function playRound(playerChoice, computerChoice) {
+let round = function playRound(playerChoice) {
+    let computerChoice = computerSelect();
     let playerWin = `Player wins! ${playerChoice} beats ${computerChoice}!`;
     let computerWin = `Computer wins! ${computerChoice} beats ${playerChoice}!`;
     
@@ -30,7 +32,7 @@ function playRound(playerChoice, computerChoice) {
     } else if (playerChoice === "rock") {
         if (computerChoice === "paper") {
             computerScore++
-            return computerWin;
+            console.log(computerWin);
         }
         else {
             playerScore++
@@ -57,36 +59,3 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
-function testRound() {
-    let player = computerSelect();
-    let cpu = computerSelect();
-    return playRound(player, cpu);
-}
-
-function game() {
-    let cpu = computerSelect();
-    let player = playerSelect();
-    let result = playRound(player,cpu);
-    console.log(result);
-    displayScores();
-}
-
-function displayScores() {
-    console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`);
-}
-
-function displayWinner() {
-    if (playerScore === 3) {
-        alert("Player has won!");
-    } else if (computerScore === 3) 
-    {
-        alert("Computer has won!");
-    }
-}
-
-
-
-let playerScore = 0;
-let computerScore = 0;
-
-// GAME LOGIC
